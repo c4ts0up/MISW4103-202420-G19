418d786266117b01b981cfcca1f805e7c5eaf48d
alvaro <a.baccap@uniandes.edu.co>
2024-11-14 18:12:50 -0500

    [WIP] Auth adjusted

--- PlaywrightAlvaro/tests/auth.setup.ts
+++ PlaywrightAlvaro/tests/auth.setup.ts
@@ -1,13 +1,14 @@
 import { test as setup, expect } from '@playwright/test';
 import * as path from 'path';
+import {adminData} from "./data/admin";
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
 setup('authenticate', async ({ page }) => {
     // Perform authentication steps. Replace these actions with your own.
     await page.goto('http://localhost:3001/ghost/#/signin');
-    await page.getByLabel('Email address').fill('username');
-    await page.getByLabel('Password').fill('password');
+    await page.getByLabel('Email address').fill(adminData.username);
+    await page.getByLabel('Password').fill(adminData.password);
     await page.getByRole('button', { name: 'Sign in' }).click();
     // Wait until the page receives the cookies.
     //
@@ -15,7 +16,7 @@ setup('authenticate', async ({ page }) => {
     // Wait for the final URL to ensure that the cookies are actually set.
     await page.waitForURL('http://localhost:3001/ghost/#/dashboard');
     // Alternatively, you can wait until the page reaches a state where all cookies are set.
-    await expect(page.getByRole('button', { name: 'View profile and more' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
 
     // End of authentication steps.
 
