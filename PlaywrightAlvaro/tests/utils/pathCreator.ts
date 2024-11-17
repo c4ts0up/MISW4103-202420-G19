9e1751bb1ffccc326d6d7a9fe1a3ae77f83bd09e
alvaro <a.baccap@uniandes.edu.co>
2024-11-16 20:57:14 -0500

    [WIP] Creación de paths personalizados

---
+++ PlaywrightAlvaro/tests/utils/pathCreator.ts
@@ -0,0 +1,33 @@
+/**
+ * ## File Utils
+ *
+ * Utilidades para el manejo de archivos
+ */
+
+
+/**
+ * Función base para crear directorios
+ * @param directories lista de directorios
+ */
+function concatanatePaths(directories: string[]) {
+    return "./" + directories.join('/')
+}
+
+/**
+ * Crea el path para un screenshot
+ * @param basePath path base para los screenshots (e.g `./results`)
+ * @param version versión de la app (e.g `5.96`)
+ * @param browserName nombre del navegador (e.g `chromium`)
+ * @param testName nombre de la prueba (e.g `E000 - Mi primera prueba`)
+ * @param screenshotIdentifier identificador sin extensión del screenshot (e.g `after`)
+ * @returns string path para guardar el screenshot
+ */
+export function screenshotPath(
+    basePath: string,
+    version: string,
+    browserName: string,
+    testName: string,
+    screenshotIdentifier: string
+): string {
+    return concatanatePaths([basePath, version, browserName, testName, `${screenshotIdentifier}.png`]);
+}
