45a8e68df5a0fa5f5b8f8da9e378ef46538cd902
alvaro <a.baccap@uniandes.edu.co>
2024-11-24 10:19:02 -0500

    [WIP] Regex for random invalid gen

--- PlaywrightSuite/tests/data/memberProvider.ts
+++ PlaywrightSuite/tests/data/memberProvider.ts
@@ -1,24 +1,23 @@
 import {faker} from "@faker-js/faker";
 
-enum NAME_GENERATION_OPTIONS {
-    LONG,
-    SHORT,
-    SPECIAL_CHARACTERS
+export enum NAME_GENERATION_OPTIONS {
+    LONG = `/^[a-zA-Z]{91} [a-zA-Z]{100}$/`,
+    SHORT = `/^$/`
 }
 
-enum EMAIL_GENERATION_OPTIONS {
-    LONG,
-    SHORT,
-    NO_AT,
-    NO_DOMAIN,
-    SPECIAL_CHARACTERS
+export enum EMAIL_GENERATION_OPTIONS {
+    LONG = `/^[a-zA-Z0-9]{186}@[a-zA-Z]\.[a-zA-Z]{3}$/`,
+    SHORT = `/^$/`,
+    NO_AT = `/^[a-zA-Z0-9]{3,50}(\\.[a-zA-Z]{2,3})+$/`,
+    NO_DOMAIN = `/^[a-zA-Z0-9]{3,50}@[a-zA-Z]{3,10}$/`,
+    SPECIAL_CHARACTERS = `^[\x00-\x7F]{3,20}@[a-zA-Z]{3,20}\\.[a-zA-Z]{2,3}$`
 }
 
 
 /**
  * Proveedor de datos para un miembro
  */
-interface MemberProvider {
+export interface MemberProvider {
 
     /**
      * Nombre válido de un miembro
@@ -47,7 +46,7 @@ interface MemberProvider {
 /**
  * Obtiene los datos de manera completamente aleatoria y sin relación
  */
-class MemberRandomProvider implements MemberProvider {
+export class MemberRandomProvider implements MemberProvider {
 
     getInvalidEmail(option: EMAIL_GENERATION_OPTIONS): string {
         if (option == EMAIL_GENERATION_OPTIONS.LONG) {
@@ -72,15 +71,7 @@ class MemberRandomProvider implements MemberProvider {
     }
 
     getInvalidName(option: NAME_GENERATION_OPTIONS): string {
-        if (option == NAME_GENERATION_OPTIONS.LONG) {
-            return `${faker.string.alpha(186)}@${faker.string.alpha(1)}.${faker.string.alpha(3)}`;
-        }
-        else if (option == NAME_GENERATION_OPTIONS.SHORT) {
-            throw new Error(`No se puede generar un nombre inválido al ser corto`);
-        }
-        else {
-            return `${faker.string.sample(186)}@${faker.string.sample(1)}.${faker.string.sample(3)}`
-        }
+        return faker.helpers.fromRegExp(NAME_GENERATION_OPTIONS[option]);
     }
 
     getValidEmail(): string {
@@ -96,7 +87,7 @@ class MemberRandomProvider implements MemberProvider {
 /**
  * Obtiene los datos de manera pseudoaleatoria. Los datos tienen relación entre ellos
  */
-class MemberRelatedProvider implements MemberProvider {
+export class MemberRelatedProvider implements MemberProvider {
 
     getInvalidEmail(option: EMAIL_GENERATION_OPTIONS): string {
         return "";
@@ -119,7 +110,7 @@ class MemberRelatedProvider implements MemberProvider {
 /**
  * Obtiene los datos almacenados para las pruebas
  */
-class MemberAPrioriProvider implements MemberProvider {
+export class MemberAPrioriProvider implements MemberProvider {
 
     getInvalidEmail(option: EMAIL_GENERATION_OPTIONS): string {
         return "";
