db8a1cab22c06bdc54b5e1c18fcd69f426daca4d
alvaro <a.baccap@uniandes.edu.co>
2024-11-24 10:38:52 -0500

    [WIP] Settings Provider (with Language)

---
+++ PlaywrightSuite/tests/data/settingsProvider.ts
@@ -0,0 +1,78 @@
+import {faker} from "@faker-js/faker";
+import * as CountryLanguage from "@ladjs/country-language";
+
+export enum LANGUAGE_GENERATION_OPTIONS {
+    LONG = `/^[a-zA-Z]{4096}$/`,
+    SHORT = `/^$/`,
+}
+
+
+/**
+ * Proveedor de datos para un
+ */
+export interface SettingsProvider {
+
+    /**
+     * Lenguaje válido
+     * Vagamente basado en [ISO 639](https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes)
+     */
+    getValidLanguage(): string
+
+    /**
+     * Lenguaje inválido
+     * Vagamente basado en [ISO 639](https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes)
+     * @param option qué característica invalidará al lenguaje dado
+     */
+    getInvalidLanguage(option: LANGUAGE_GENERATION_OPTIONS): string
+}
+
+
+/**
+ * Obtiene los datos de manera completamente aleatoria y sin relación
+ */
+export class SettingsRandomProvider implements SettingsProvider {
+    getInvalidLanguage(option: LANGUAGE_GENERATION_OPTIONS): string {
+        return faker.helpers.fromRegExp(LANGUAGE_GENERATION_OPTIONS[option]);
+    }
+
+    getValidLanguage(): string {
+        const codes = [
+            ...CountryLanguage.getLanguageCodes(1),
+            ...CountryLanguage.getLanguageCodes(2),
+            ...CountryLanguage.getLanguageCodes(3),
+        ]
+
+        return faker.helpers.arrayElement(codes);
+    }
+
+}
+
+
+/**
+ * Obtiene los datos de manera pseudoaleatoria. Los datos tienen relación entre ellos
+ */
+export class SettingsRelatedProvider implements SettingsProvider {
+
+    getValidLanguage(): string {
+        return "";
+    }
+
+    getInvalidLanguage(option: LANGUAGE_GENERATION_OPTIONS): string {
+        return "";
+    }
+}
+
+
+/**
+ * Obtiene los datos almacenados para las pruebas
+ */
+export class SettingsAPrioriProvider implements SettingsProvider {
+
+    getInvalidLanguage(option: LANGUAGE_GENERATION_OPTIONS): string {
+        return "";
+    }
+
+    getValidLanguage(): string {
+        return "";
+    }
+}
