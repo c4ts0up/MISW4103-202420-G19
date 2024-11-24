529ddfeaebe8deda8af0623b5bea86dac9bb071d
alvaro <a.baccap@uniandes.edu.co>
2024-11-24 09:25:50 -0500

    [WIP] MemberDataProvider random generator

--- PlaywrightSuite/tests/data/memberProvider.ts
+++ PlaywrightSuite/tests/data/memberProvider.ts
@@ -1,3 +1,20 @@
+import {faker} from "@faker-js/faker";
+
+enum NAME_GENERATION_OPTIONS {
+    LONG,
+    SHORT,
+    SPECIAL_CHARACTERS
+}
+
+enum EMAIL_GENERATION_OPTIONS {
+    LONG,
+    SHORT,
+    NO_AT,
+    NO_DOMAIN,
+    SPECIAL_CHARACTERS
+}
+
+
 /**
  * Proveedor de datos para un miembro
  */
@@ -10,8 +27,9 @@ interface MemberProvider {
 
     /**
      * Nombre inválido de un miembro
+     * @param option qué característica invalidará al nombre dado
      */
-    getInvalidName(): string
+    getInvalidName(option: NAME_GENERATION_OPTIONS): string
 
     /**
      * Email válido de un miembro
@@ -20,8 +38,9 @@ interface MemberProvider {
 
     /**
      * Email inválido de un miembro
+     * @param option qué característica invalidará al nombre dado
      */
-    getInvalidEmail(): string
+    getInvalidEmail(option: EMAIL_GENERATION_OPTIONS): string
 }
 
 
@@ -30,20 +49,46 @@ interface MemberProvider {
  */
 class MemberRandomProvider implements MemberProvider {
 
-    getInvalidEmail(): string {
-        return "";
+    getInvalidEmail(option: EMAIL_GENERATION_OPTIONS): string {
+        if (option == EMAIL_GENERATION_OPTIONS.LONG) {
+            // aaa...a@b.xyz
+            return `${faker.string.alphanumeric(186)}@${faker.string.alpha(1)}.${faker.string.alpha(3)}`;
+        }
+        else if (option == EMAIL_GENERATION_OPTIONS.SHORT) {
+            return "";
+        }
+        else if (option == EMAIL_GENERATION_OPTIONS.NO_AT) {
+            // abcdefgh.xyz
+            return `${faker.string.alphanumeric()}.${faker.string.alpha(3)}`
+        }
+        else if (option == EMAIL_GENERATION_OPTIONS.NO_DOMAIN) {
+            // abcdefgh@mail
+            return `${faker.string.alphanumeric()}@${faker.string.alpha()}`
+        }
+        else {
+            // ñññ...ñ@mail.xyz
+            return `${faker.string.sample()}@${faker.string.alpha()}.${faker.string.sample(3)}`
+        }
     }
 
-    getInvalidName(): string {
-        return "";
+    getInvalidName(option: NAME_GENERATION_OPTIONS): string {
+        if (option == NAME_GENERATION_OPTIONS.LONG) {
+            return `${faker.string.alpha(186)}@${faker.string.alpha(1)}.${faker.string.alpha(3)}`;
+        }
+        else if (option == NAME_GENERATION_OPTIONS.SHORT) {
+            throw new Error(`No se puede generar un nombre inválido al ser corto`);
+        }
+        else {
+            return `${faker.string.sample(186)}@${faker.string.sample(1)}.${faker.string.sample(3)}`
+        }
     }
 
     getValidEmail(): string {
-        return "";
+        return faker.internet.email();
     }
 
     getValidName(): string {
-        return "";
+        return faker.person.fullName();
     }
 }
 
@@ -53,11 +98,11 @@ class MemberRandomProvider implements MemberProvider {
  */
 class MemberRelatedProvider implements MemberProvider {
 
-    getInvalidEmail(): string {
+    getInvalidEmail(option: EMAIL_GENERATION_OPTIONS): string {
         return "";
     }
 
-    getInvalidName(): string {
+    getInvalidName(option: NAME_GENERATION_OPTIONS): string {
         return "";
     }
 
@@ -76,11 +121,11 @@ class MemberRelatedProvider implements MemberProvider {
  */
 class MemberAPrioriProvider implements MemberProvider {
 
-    getInvalidEmail(): string {
+    getInvalidEmail(option: EMAIL_GENERATION_OPTIONS): string {
         return "";
     }
 
-    getInvalidName(): string {
+    getInvalidName(option: NAME_GENERATION_OPTIONS): string {
         return "";
     }
 
