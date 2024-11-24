d471161d8f1aa8b547c504696c6a560c5ec0e7ee
alvaro <a.baccap@uniandes.edu.co>
2024-11-24 13:50:00 -0500

    [WIP] PostRandomProvider implementado. Falta probar

--- PlaywrightSuite/tests/data/postProvider.ts
+++ PlaywrightSuite/tests/data/postProvider.ts
@@ -58,25 +58,59 @@ export interface PostProvider {
  */
 export class PostRandomProvider implements PostProvider {
     getValidTitle(): string {
-        throw new Error("Method not implemented.");
+        return faker.lorem.sentences({min: 1, max: 2});
     }
     getValidContent(): string {
-        throw new Error("Method not implemented.");
+        return faker.lorem.paragraphs({min: 1, max: 5});
     }
     getValidDate(): string {
-        throw new Error("Method not implemented.");
+        const futureDate = faker.date.future({years: 1});
+        return `${futureDate.getFullYear()}-${futureDate.getMonth()}-${futureDate.getDate()}`;
     }
     getValidTime(): string {
-        throw new Error("Method not implemented.");
+        const futureDate = faker.date.future({years: 1});
+        return `${futureDate.getHours()}:${futureDate.getMinutes()}`;
     }
     getInvalidDate(option: DATE_GENERATION_OPTIONS): string {
-        throw new Error("Method not implemented.");
+        if (option == DATE_GENERATION_OPTIONS.PAST) {
+            const pastDate = faker.date.past({years: 1});
+            return `${pastDate.getFullYear()}-${pastDate.getMonth()}-${pastDate.getDate()}`;
+        }
+        else if (option == DATE_GENERATION_OPTIONS.OVERFLOW) {
+            const futureDate = faker.date.future({years: 1});
+            return `${futureDate.getFullYear()}-${faker.number.int({min: 13, max: 16})}-${futureDate.getDate()}`;
+        }
+        else return "2023-02-29"
     }
     getInvalidTime(option: TIME_GENERATION_OPTIONS): string {
-        throw new Error("Method not implemented.");
+        let hours: number, minutes: number;
+
+        if (option == TIME_GENERATION_OPTIONS.PAST) {
+            const now = new Date();
+            hours = faker.number.int({min: 0, max: now.getHours()});
+
+            if (hours == now.getHours()) {
+                minutes = faker.number.int({min: 0, max: now.getMinutes()-1});
+            } else {
+                minutes = faker.number.int({min: 0, max: 59});
+            }
+        }
+        else {
+            hours = faker.number.int({min: 25, max: 28});
+            minutes = faker.number.int({min: 0, max: 59});
+        }
+
+        let hoursString = "", minutesString = "";
+        // ajusta las horas
+        if (hours < 10) hoursString = `0${hours}`;
+        else hoursString = `${hours}`;
+
+        // ajusta los minutos
+        if (minutes < 10) minutesString = `0${minutes}`;
+        else minutesString = `${minutes}`;
+
+        return `${hoursString}:${minutesString}`;
     }
-
-
 }
 
 
